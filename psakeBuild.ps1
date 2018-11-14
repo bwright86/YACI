c0dff7e4086634d6eb75f354d46d5d1f81fa5e0a
Brent Wright <brentgwright@gmail.com>
2018-11-13 22:42:31 -0500

    Updated with PowershellBuild for psake tasks.

---
+++ psakeBuild.ps1
@@ -0,0 +1,21 @@
+properties {
+    # These settings overwrite values supplied form the PowerShellBuild
+    # module and govern how those tasks are executed
+    $scriptAnalysisEnabled = $true
+
+    $codeCoverageEnabled = $true
+    $codeCoverageFiles = @(
+        "$PSScriptRoot/OVFHelper/Classes/*.ps1",
+        "$PSScriptRoot/OVFHelper/Private/*.ps1",
+        "$PSScriptRoot/OVFHelper/Public/*.ps1")
+
+    $testRootDir = "$projectRoot/test"
+}
+
+Import-Module PowerShellBuild
+
+task default -depends Build
+
+task Build -FromModule PowerShellBuild
+
+task Test -FromModule PowerShellBuild
